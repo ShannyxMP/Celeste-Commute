fb7996f2bb70be59649c1d488c71fbf958175950
ShannyxMP <shannyxmp@gmail.com>
2025-05-09 21:12:34 +1000

    feat: periodically update soundtrack every hour based on current time

--- index.js
+++ index.js
@@ -5,26 +5,45 @@ const app = express();
 const port = 3000;
 const baseURL = "https://ac-api.vercel.app/";
 
-const currentTime = new Date().toLocaleTimeString([], { hour: "2-digit" }); // Output eg.: 06 PM
+let lastFetchedHour, currentSoundtrack;
+
+// To get current time:
+function getCurrentHour() {
+  return new Date().toLocaleTimeString([], {
+    hour: "2-digit",
+  }); // Output eg.: 06 PM
+}
+
+// Update song every hour:
+async function checkAndUpdateHour() {
+  const hourNow = getCurrentHour();
+
+  // [ ] Check if song updates on the next hour
+  if (hourNow !== lastFetchedHour) {
+    try {
+      const result = await axios.get(`${baseURL}api/?time=${hourNow}`);
+      currentSoundtrack = result.data.music[1].file; // Array: 0 -> for New Horizons; 1 -> New Leaf; 2 -> City Folk; 3 -> GameCube
+      lastFetchedHour = hourNow;
+    } catch (error) {
+      console.error("API fetch error: ", error.message);
+      currentSoundtrack: null;
+    }
+  }
+}
+setInterval(checkAndUpdateHour, 60000); // Check every minute
+checkAndUpdateHour(); // Run once at startup
 
 // Middlewares:
 app.use(express.static("public")); // Handle static files
 
+// Route(s):
 app.get("/", async (req, res) => {
   // console.log("You are on the homepage.");
-  try {
-    const result = await axios.get(`${baseURL}api/?time=${currentTime}`);
-    res.render("index.ejs", {
-      message: null,
-      soundtrack: result.data.music[1].file, // Array: 0 -> for New Horizons; 1 -> New Leaf; 2 -> City Folk; 3 -> GameCube
-    });
-  } catch (error) {
-    console.error(error.message);
-    res.render("index.ejs", {
-      message: "Unable to retrieve file.",
-      soundtrack: null,
-    });
-  }
+  res.render("index.ejs", {
+    message: currentSoundtrack ? null : "Unable to retrieve file.",
+    soundtrack: currentSoundtrack,
+    time: lastFetchedHour,
+  });
 });
 
 app.listen(port, () => {
