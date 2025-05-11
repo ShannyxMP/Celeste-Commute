566c8f5b7394a8edaac86212bc6f5fc582e6c93b
ShannyxMP <shannyxmp@gmail.com>
2025-05-11 20:58:27 +1000

    feat: add manual soundtrack selection with option to revert to auto mode

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import express from "express";
 import axios from "axios";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 const baseURL = "https://ac-api.vercel.app/";
+
+// Middlewares:
+app.use(express.static("public")); // Handle static files
+app.use(bodyParser.urlencoded()); // Parse incoming form data
 
 let lastFetchedHour, currentSoundtrack;
 
@@ -11,14 +16,13 @@ let lastFetchedHour, currentSoundtrack;
 function getCurrentHour() {
   return new Date().toLocaleTimeString([], {
     hour: "2-digit",
-  }); // Output eg.: 06 PM
+  }); // Output eg.: 06 pm
 }
 
 // Update song on server-side every hour:
 async function checkAndUpdateHour() {
   const hourNow = getCurrentHour();
 
-  // [x] Confirm song updates when the hour changes
   if (hourNow !== lastFetchedHour) {
     try {
       const result = await axios.get(`${baseURL}api/?time=${hourNow}`);
@@ -34,16 +38,18 @@ async function checkAndUpdateHour() {
 setInterval(checkAndUpdateHour, 30000); // Periodically check if the hour has changed; if so, fetch the new track from external API
 checkAndUpdateHour(); // Run once at startup
 
-// Middlewares:
-app.use(express.static("public")); // Handle static files
-
 // Route(s):
 app.get("/", async (req, res) => {
+  const requestedSoundtrack = req.query.manual === "true"; // === "true" checks if the value is exactly the string "true" | If the URL contains ?manual=true, then isManual will be true
+  const requestedTime = req.query.time;
   // Render homepage with initial soundtrack and time
   res.render("index.ejs", {
-    message: currentSoundtrack ? null : "Unable to retrieve file.",
+    message: currentSoundtrack
+      ? null
+      : "Unable to retrieve selected sountrack.",
     soundtrack: currentSoundtrack,
-    time: lastFetchedHour,
+    time:
+      requestedSoundtrack && requestedTime ? requestedTime : lastFetchedHour, // If requestedSoundtrack is true and requestedTime (e.g., "1PM", "9AM") is provided, use customTime for the time value passed
   });
 });
 
@@ -54,6 +60,29 @@ app.get("/current-soundtrack", (req, res) => {
     soundtrack: currentSoundtrack,
     time: lastFetchedHour,
   });
+});
+
+// [x] Handle issue with soundtrack being changed every 30secs due to /current-soundtrack -> setInterval
+app.post("/set-soundtrack", async (req, res) => {
+  // Handle incoming request to change hourly soundtrack based on selected time by client
+  const selectedTime = req.body.setTime; // Output eg.: 12AM
+  console.log(`You are changing the time to ${selectedTime}`);
+
+  if (selectedTime) {
+    try {
+      const result = await axios.get(`${baseURL}api/?time=${selectedTime}`);
+      const wantedSoundtrack = result.data.music[1].file;
+
+      // Update global variables:
+      lastFetchedHour = req.body.setTime;
+      currentSoundtrack = wantedSoundtrack;
+
+      // Redirect to homepage with manual override flag
+      res.redirect(`/?manual=true&time=${encodeURIComponent(selectedTime)}`); // encodeURIComponent() makes sure special characters (like AM, PM) in the time string are safely included in the URL
+    } catch (error) {
+      console.error("Error fetching manual soundtrack: ", error.message);
+    }
+  }
 });
 
 app.listen(port, () => {
--- public/main.js
+++ public/main.js
@@ -1,36 +1,39 @@
-// [x] Test if soundtrack changes as hour is updated:
-
 // Client-side polling every 30 seconds to check if a new hour has begun
-setInterval(async () => {
-  try {
-    const res = await fetch("/current-soundtrack");
-    const data = await res.json();
 
-    const audioElement = document.querySelector("#audioPlayer");
-    const audioSource = document.querySelector("#audioPlayer source");
+const songWasRequested = window.location.search.includes("manual=true"); // Check if manual override is active in the URL
 
-    // To retrieve relative URLs/paths to avoid false positives from absolute paths:
-    const currentSoundtrack = new URL(audioSource.src).pathname;
-    const newSoundtrack = new URL(data.soundtrack, window.location.origin) // window.location.origin required as the base URL (e.g., http://localhost:3000) to build a proper absolute URL - no base may cause error when passed directly to new URL(...)
-      .pathname;
+if (!songWasRequested) {
+  setInterval(async () => {
+    try {
+      const res = await fetch("/current-soundtrack");
+      const data = await res.json();
 
-    console.log(
-      `Currently playing: ${currentSoundtrack}. If ${newSoundtrack} pathname is different, it will be the new song to be played.`
-    );
+      const audioElement = document.querySelector("#audioPlayer");
+      const audioSource = document.querySelector("#audioPlayer source");
 
-    // Update soundtrack if server-fetched audio differs from the one currently playing
-    if (currentSoundtrack !== newSoundtrack) {
-      audioSource.src = data.soundtrack;
-      audioElement.load();
-      if (audioElement.play()) {
-        audioElement.play();
+      // To retrieve relative URLs/paths to avoid false positives from absolute paths:
+      const currentSoundtrack = new URL(audioSource.src).pathname;
+      const newSoundtrack = new URL(data.soundtrack, window.location.origin) // window.location.origin required as the base URL (e.g., http://localhost:3000) to build a proper absolute URL - no base may cause error when passed directly to new URL(...)
+        .pathname;
+
+      console.log(
+        `Currently playing: ${currentSoundtrack}. If ${newSoundtrack} pathname is different, it will be the new song to be played.`
+      );
+
+      // Update soundtrack if server-fetched audio differs from the one currently playing
+      if (currentSoundtrack !== newSoundtrack) {
+        audioSource.src = data.soundtrack;
+        audioElement.load();
+        if (audioElement.play()) {
+          audioElement.play();
+        }
+        console.log(`Soundtrack changed to: ${audioSource.src}`);
       }
-      console.log(`Soundtrack changed to: ${audioSource.src}`);
-    }
 
-    // Update time display
-    document.querySelector("#displayTime").textContent = data.time;
-  } catch (error) {
-    console.error("API fetch error: ", error.message);
-  }
-}, 30000); // To fetch data every 30secs
+      // Update time display
+      document.querySelector("#displayTime").textContent = data.time;
+    } catch (error) {
+      console.error("API fetch error: ", error.message);
+    }
+  }, 30000); // To fetch data every 30secs
+}
